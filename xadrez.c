9d0afa2cf9e9059d07208090b8ba843da036101c
RedSword-XT <202504507451@alunos.estacio.br>
2025-05-22 20:49:50 +0000

    Nível Aventureiro - Movimento do cavalo

--- xadrez.c
+++ xadrez.c
@@ -2,31 +2,49 @@
 
 int main() { 
 
-    // Nível novato - Movimentação das Peças
+    // Nível Novato - Movimentação das Peças
 
-    int torre = 0;   // Contador para a torre
-    int bispo = 0;   // Contador para o bispo
-    int rainha = 0;  // Contador para a rainha
+    int i, j;
 
-    // Torre: anda 5 casas para a direita
+    // Movimento da TORRE - 5 casas para a direita
     printf("Movimento da Torre:\n");
-    for (torre = 0; torre < 5; torre++) {
+    for (i = 0; i < 5; i++) {
         printf("Direita\n");
     }
 
-    // Bispo: anda 5 casas na diagonal (cima e direita)
+    // Movimento do BISPO - 5 casas na diagonal para cima e à direita
     printf("\nMovimento do Bispo:\n");
-    while (bispo < 5) {
+    i = 0;
+    while (i < 5) {
         printf("Cima, Direita\n");
-        bispo++;
+        i++;
     }
 
-    // Rainha: anda 8 casas para a esquerda
+    // Movimento da RAINHA - 8 casas para a esquerda
     printf("\nMovimento da Rainha:\n");
+    i = 0;
     do {
         printf("Esquerda\n");
-        rainha++;
-    } while (rainha < 8);
+        i++;
+    } while (i < 8);
+
+
+    //Nível Aventureiro - Movimentação do Cavalo
+
+    
+    // Movimento do CAVALO - 2 casas para baixo e 1 para a esquerda
+    printf("\nMovimento do Cavalo:\n");
+    for (i = 0; i < 2; i++) { // 2 passos para baixo
+        printf("Baixo\n");
+
+        if (i == 1) {// Depois do segundo passo, faz 1 passo para a esquerda
+            j = 0;
+            while (j < 1) {
+                printf("Esquerda\n");
+                j++;
+            }
+        }
+    }
 
     return 0;
 }
